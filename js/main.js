b51d1d9bb9b9e00f6f342b110d7238f3db668c63
ch1kim0n1 <110909601+ch1kim0n1@users.noreply.github.com>
2022-11-06 16:39:23 -0600

    remove /challenge from links

--- js/main.js
+++ js/main.js
@@ -2,57 +2,57 @@ let listItems = [
     {
         title: "Very first anime you watched",
         link: "https://anilist.co/activity/464575970",
-        image: "images/challenge/1.1.jpg"
+        image: "images/1.1.jpg"
     },
     {
         title: "Favorite anime you've watched so far",
         link: "https://anilist.co/activity/464958093",
-        image: "images/challenge/2.1.2019.jpg"
+        image: "images/2.1.2019.jpg"
     },
     {
         title: "Favorite male anime character ever",
         link: "https://anilist.co/activity/465340663",
-        image: "images/challenge/3.1.jpg"
+        image: "images/3.1.jpg"
     },
     {
         title: "Favorite female anime character ever",
         link: "https://anilist.co/activity/465784021",
-        image: "images/challenge/4.png"
+        image: "images/4.png"
     },
     {
         title: "Anime I'm ashamed I enjoyed",
         link: "https://anilist.co/activity/466207484",
-        image: "images/challenge/5.jpg"
+        image: "images/5.jpg"
     },
     {
         title: "Anime I want to see but haven't yet",
         link: "https://anilist.co/activity/466630832",
-        image: "images/challenge/6.1.jpg"
+        image: "images/6.1.jpg"
     },
     {
         title: "My anime crush",
         link: "https://anilist.co/activity/467134426",
-        image: "images/challenge/7.jpg"
+        image: "images/7.jpg"
     },
     {
         title: "Favorite anime couple",
         link: "https://anilist.co/activity/467618427",
-        image: "images/challenge/8.2.png"
+        image: "images/8.2.png"
     },
     {
         title: "Best Anime Villian",
         link: "https://anilist.co/activity/467921021",
-        image: "images/challenge/9.png" //day 9
+        image: "images/9.png" //day 9
     },
     {
         title: "Favorite Fighter Anime",
         link: "https://anilist.co/activity/468522251",
-        image: "images/challenge/10.jpg"
+        image: "images/10.jpg"
     },
     {
         title: "Favorite mecha anime",
         link: "https://anilist.co/activity/468995371",
-        image: "images/challenge/11.jpg" //day 11
+        image: "images/11.jpg" //day 11
     }
 ]
 
